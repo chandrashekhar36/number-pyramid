dbf2da353f0203b4a4ed4cd6c56f18a40283fffc
Chandrashekhar B <115780527+chandrashekhar36@users.noreply.github.com>
2023-01-14 22:39:09 +0530

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class pyramid
+{
+  public static void main(String args[])
+  {
+    Scanner scanner = new Scanner(System.in);
+    int n;
+    System,out.println("enter a pyramid size");
+    n=(int)scanne.nextInt();
+    for (int a=1; a<=n; a++)
+    {
+      for (int b=1; b<=(n-a)*2; b++)
+      {
+        System.out.print(" ");
+      }
+      for (int k=1; k<=i; k++)
+      {
+        System.out.print("   "+i);
+      }
+      System.out.print();
+    }
+  }
+}
